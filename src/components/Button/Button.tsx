44dbf0adb1f1b859fdb5ff2b9f2b2b8395ef8dce
amellor <amellor@ampol.com.au>
2022-08-15 18:14:21 +1000

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 import styles from './Button.module.scss'
 import Image from 'next/image'
 
-const Button = ({children, image} : { children: ReactNode, image?: string}) => {
+const Button = ({children, image} : { children?: ReactNode, image?: string}) => {
   return (
     <button className={styles.button}>
       {image && <Image src={image} width={20} height={20} alt="logo"/>}
