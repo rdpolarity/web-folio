eb683d6d0fe2ff92812f682f33ea6f746030724e
amellor <amellor@ampol.com.au>
2022-08-23 21:58:40 +1000

    Firebase GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,25 @@ import AppLayout from "@components/Layout";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { globalStore, GlobalStoreProvider } from "stores/GlobalStore";
-import Analytics from 'analytics';
-import googleAnalytics from '@analytics/google-analytics';
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
-export const analytics = Analytics({
-  app: 'aydie.me',
-  plugins: [
-    googleAnalytics({
-      measurementIds: ['G-WTM930C6WK']
-    })
-  ]
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyAWbHb0tOgKZe5dZwjjN3P3w43zfibPf_Y",
+  authDomain: "aydieme-2da8c.firebaseapp.com",
+  projectId: "aydieme-2da8c",
+  storageBucket: "aydieme-2da8c.appspot.com",
+  messagingSenderId: "356919318550",
+  appId: "1:356919318550:web:bdbdf05e4273b22915bd37",
+  measurementId: "G-F9MQRWECTK"
+};
 
-analytics.page();
+// Initialize Firebase
+if (typeof document !== 'undefined') {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  logEvent(analytics, 'page_view')
+}
 
 const queryClient = new QueryClient();
 
