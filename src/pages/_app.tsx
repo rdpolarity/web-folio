8bd44555238312e91b0208b133242dbea9d0319e
amellor <amellor@ampol.com.au>
2022-08-23 17:59:36 +1000

    Added Google Analytics
    Update GlobalStore.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,18 @@ import AppLayout from "@components/Layout";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { globalStore, GlobalStoreProvider } from "stores/GlobalStore";
+import { useEffect } from "react";
+import ReactGA from 'react-ga';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  useEffect(() => {
+    ReactGA.initialize('G-WTM930C6WK');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+  
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStoreProvider value={globalStore}>
--- src/stores/GlobalStore.tsx
+++ src/stores/GlobalStore.tsx
@@ -1,8 +1,8 @@
-import { computed, makeAutoObservable } from "mobx";
+import { autorun, computed, makeAutoObservable } from "mobx";
+import ReactGA from "react-ga";
 import React from "react";
 
 class GlobalStore {
-
   isMinecraft = false;
   isMinecraftFound = false;
 
@@ -12,27 +12,51 @@ class GlobalStore {
   isSpyroFound = false;
 
   @computed
-  getEasterEggs = () => [this.isMinecraftFound, this.isDotsFound, this.isSpyroFound];
+  getEasterEggs = () => [
+    this.isMinecraftFound,
+    this.isDotsFound,
+    this.isSpyroFound,
+  ];
 
   @computed
-  getEasterEggCount = () => this.getEasterEggs().filter(e => e).length;
+  getEasterEggCount = () => this.getEasterEggs().filter((e) => e).length;
 
   constructor() {
     makeAutoObservable(this);
   }
 
   setIsDots(isDots: boolean) {
-    if (isDots) this.isDotsFound = true;
+    if (isDots) {
+      this.isDotsFound = true;
+      ReactGA.event({
+        category: "Easter Eggs",
+        action: "Dots secret Found",
+        value: this.getEasterEggCount(),
+      });
+    }
     this.isDots = isDots;
   }
 
   setIsMinecraft(isMinecraft: boolean) {
-    if (isMinecraft) this.isMinecraftFound = true;
+    if (isMinecraft) {
+      this.isMinecraftFound = true;
+
+      ReactGA.event({
+        category: "Easter Eggs",
+        action: "Minecraft secret found",
+        value: this.getEasterEggCount(),
+      });
+    }
     this.isMinecraft = isMinecraft;
   }
 
   spyroFound() {
     this.isSpyroFound = true;
+    ReactGA.event({
+      category: "Easter Eggs",
+      action: "Spyro secret found",
+      value: this.getEasterEggCount(),
+    });
   }
 }
 
