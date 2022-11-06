667b3ccce64a9506d8a0f8cf4a0b0576b0d16966
amellor <amellor@ampol.com.au>
2022-11-06 23:18:35 +1100

    feat: Improved design of project cards

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import { TagEntity } from "@api/generated/api";
-import { Card, Tag } from "antd";
 import React from "react";
 import styles from "./Project.module.scss";
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
+import { Badge, Card, Text } from "@nextui-org/react";
 
 interface ProjectProps {
   title?: string | null;
@@ -12,34 +12,65 @@ interface ProjectProps {
 }
 
 const Project = (props: ProjectProps) => {
+  
   return (
-    <Tilt glareEnable tiltMaxAngleX={3} tiltMaxAngleY={3} glareMaxOpacity={0.5}>
-    <div className={styles.project}>
-      <h4 className={styles.projectTitle}>{props.title}</h4>
-      <div className={styles.projectThumbnail}>
-        <img src={props.thumbnail} alt="thumbnail" />
-        <div className={styles.projectTags}>
+    <Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} glareMaxOpacity={0.5}>
+      <Card
+        className={styles.project}
+        css={{
+          width: 230,
+          maxHeight: 300,
+        }}
+        isPressable
+      >
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 0, p: 0, h: 100, alignItems: 'start' }}>
+          <div className={styles.projectGradient}/>
+          <Text size={18} weight="bold" transform="uppercase" color="white" css={{ p: 10, top: 10 }}>
+            {props.title}
+          </Text>
+        </Card.Header>
+        <Card.Body css={{ p: 0 }}>
+          <Card.Image
+            height={260}
+            width="100%"
+            objectFit="cover"
+            alt="Card Background"
+            src={props.thumbnail ?? ""}
+          />
+        </Card.Body>
+        <Card.Footer
+          isBlurred
+          className={styles.projectFooter}
+          css={{
+            position: "absolute",
+            bgBlur: "#ffffff66",
+            borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+            bottom: 0,
+            zIndex: 1,
+            gap: 5,
+            flexWrap: "wrap",
+          }}
+        >
           {props.tags?.map((tag: any, index: any) => (
-            <Tag
-              icon={
-                tag.attributes?.icon?.data?.attributes?.url && (
-                  <img
-                    className={styles.projectTagsIcon}
-                    src={tag.attributes?.icon?.data?.attributes?.url}
-                    alt={tag.attributes?.name}
-                  />
-                )
-              }
+            <Badge
+              enableShadow
               style={{ margin: 0 }}
               key={index}
-              color={tag.attributes?.colour ?? "default"}
+              disableOutline
+              color="primary"
             >
+              {tag.attributes?.icon?.data?.attributes?.url && (
+                <img
+                  className={styles.projectTagsIcon}
+                  src={tag.attributes?.icon?.data?.attributes?.url}
+                  alt={tag.attributes?.name}
+                />
+              )}
               {tag.attributes?.name}
-            </Tag>
+            </Badge>
           ))}
-        </div>
-      </div>
-    </div>
+        </Card.Footer>
+      </Card>
     </Tilt>
   );
 };
